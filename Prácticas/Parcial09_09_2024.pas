1dcc8a9c061b3e80f5ffb31a6f47d4b28dd1470d
ezequielbaamonde <ezequielitobaamonde@gmail.com>
2024-12-02 00:20:23 -0300

    Parcial terminado

--- Prácticas/Parcial09_09_2024.pas
+++ Prácticas/Parcial09_09_2024.pas
@@ -34,7 +34,8 @@ procedure leerEnvio(var e: envio);
  begin
 	e.codCli:= Random(100);
 	if (e.codCli <> 0) then begin
-		e.cp:= Random(5);
+		{e.cp:= 1000 + (random(3000) + 1);}
+		e.cp:= random(10);
 		e.dia:= 1 + random(30);
 		e.peso:= Random(20000)/(Random(10)+1);
 	end;
@@ -96,19 +97,72 @@ procedure generarArbol(var a: arbol);
 
 procedure recorrerArbol(a: arbol);
  begin
-	if (a<>NIL) then begin	
+	if (a<>NIL) then begin
+		recorrerArbol(a^.HI);
 		writeln('CP: ', a^.cp);
-		recorrerArbol(a^.HI);
-		recorrerArbol(a^.HI);
+		recorrerArbol(a^.HD);
 	end;
  end;
 
+procedure retEnv(a: arbol; var lis: listaEnvios; c: integer);
+ begin
+	if (a <> NIL) then begin
+		if (a^.cp = c) then begin
+			lis:= a^.lista;
+		end;
+		retEnv(a^.HI, lis, c);
+		retEnv(a^.HD, lis, c);
+	end;	
+ end;
 
-var abb1: arbol;
+procedure recorrerLista(lis: listaEnvios);
+ begin
+	while(lis <> NIL) do begin
+		writeln('Cod Cliente ', lis^.dato.codCli, ', peso: ', lis^.dato.peso:1:2);
+		lis:= lis^.sig;
+	end;
+ end;
+
+procedure doscodigos(lis: listaEnvios; var max, min: real; var cMax, cMin: integer);
+ begin
+	if (lis <> NIl) then begin
+		if (lis^.dato.peso > max) then begin
+			max:= lis^.dato.peso;
+			cMax:= lis^.dato.codCli
+		end
+		else
+		 begin
+			if (lis^.dato.peso < min) then begin
+				min:= lis^.dato.peso;
+				cMin:= lis^.dato.codCli;
+			end;
+		 end;
+		
+		doscodigos(lis^.sig, max, min, cMax, cMin);
+	end;
+ end;
+
+var abb1: arbol; max, min: real; cp, cMax, cMin: integer; lisEnv: listaEnvios;
 begin
  randomize;
  //a.
  abb1:= NIL;
  generarArbol(abb1);
+ writeln('*Mostrando ARBOL....*');
  recorrerArbol(abb1);
+ writeln();
+ //b.
+ cp:= 2;
+ lisEnv:= NIL;
+ retEnv(abb1, lisEnv, cp);
+ writeln('*Mostrando INCISO B....*');
+ recorrerLista(lisEnv);
+ writeln();
+ //c.
+ max:= -1; min:= 9999;
+ cMax:= 0; cMin:= 0;
+ doscodigos(lisEnv, max, min, cMax, cMin);
+ writeln('*Mostrando INCISO C...*');
+ writeln('Codigo del cliente con mayor peso: ', cMax);
+ writeln('Codigo del cliente con menor peso: ', cMin);
 end.
